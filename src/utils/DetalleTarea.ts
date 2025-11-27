7aa89b01eb1262b0774ba06d8881d9a02a566769
maia2303 <bulaciomaia23@gmail.com>
2025-11-27 18:58:29 -0300

    comentarios

--- src/utils/DetalleTarea.ts
+++ src/utils/DetalleTarea.ts
@@ -1,8 +1,9 @@
 import { Tarea } from "../models/Tarea";
 
 export const mostrarDetalle = (t: Tarea) => {
-     
+    // guardamos la funcion en una constante
     const dificultadLuna = "🌕".repeat(t.dificultad) + "🌑".repeat(3 - t.dificultad);
+    //creamos una variable para que muestre la dificultad según lo que ponemos en consola
 
     console.log(`
     ID: ${t.id} \n
